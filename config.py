7434e444846731891fe8771cb40ea37244400fe7
hruthikvs <52667241+hruthikvs@users.noreply.github.com>
2021-11-26 16:16:24 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -10,5 +10,12 @@ config = {"acrobot": [0, 0], "taxi": [0, 0], "kbca": [0, 0], "kbcb": [0, 0], "kb
 
 env = aicrowd_gym.make("Taxi-v3")
 config['taxi'] = [env.observation_space.n,env.action_space.n]
-print(config)
+env = aicrowd_gym.make("gym_bellman:kbc-a-v0")
+config['kbca'] = [env.N+1,env.action_space.n]
 
+env = aicrowd_gym.make("gym_bellman:kbc-b-v0")
+config['kbcb'] = [env.N+1,env.action_space.n]
+env = aicrowd_gym.make("gym_bellman:kbc-c-v0")
+config['kbcc'] = [env.N+1,env.action_space.n]
+
+print(config)
